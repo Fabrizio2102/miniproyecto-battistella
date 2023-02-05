53dd6152fa6c41942c155b08b7f704d1757b9882
Fabrizio Battistella <f.battistella@gmail.com>
2023-02-05 13:24:12 -0400

    create contact

--- main.js
+++ main.js
@@ -1,6 +1,43 @@
 const grande = document.querySelector(".grande")
 const punto = document.querySelectorAll(".punto")
 const barra = document.querySelectorAll(".barra")
+const nombre = document.getElementById("nombre")
+const email = document.getElementById("email")
+const msg = document.getElementById("mensaje")
+const enviar = document.getElementById("enviar")
+
+
+
+const campos = {
+    nombre: false,
+    email: false,
+    mensaje: false
+}
+
+enviar.addEventListener('click', ()=>{
+    validar();
+})
+
+function validar(){
+    if(nombre.value != ""){
+        campos.nombre = true;
+    }
+    for(i=0; i<email.value.length; i++){
+        if(email.value[i] == "@"){
+            campos.email = true;
+        }
+    }
+    if(msg.value != ""){
+        campos.mensaje = true;
+    }
+
+    if(campos.nombre && campos.email && campos.mensaje){
+        alert("Se ha enviado el mensaje.")
+    }else{
+        alert("ERROR. No se ha podido enviar el mensaje.")
+    }
+}
+
 
 const habilidades = {
     programacion: 88,
